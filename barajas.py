c4c12e07fcaed27744ded76ce90f7312b22a94cf
Miguel Arroyo Calvo <miguelarroyocalvo@MacBook-Pro-de-Miguel.local>
2023-05-17 23:14:58 +0200

    feat: barajas terminado

--- barajas.py
+++ barajas.py
@@ -19,3 +19,28 @@ def barajar_for(baraja):
         baraja [i] , baraja[j] = baraja [j], baraja [i]
     
     return baraja
+
+class Baraja:
+    def __init__(self, numeros, palos):
+        self.naipes = []
+        for palo in palos:
+            for numero in numeros:
+                naipe = numero + palo
+                self.naipes.append(naipe)
+
+    def barajar(self):
+        for i in range(len(self.naipes)):
+            j = random.randrange(len(self.naipes))
+            self.naipes[i], self.naipes[j] = self.naipes[j], self.naipes[i]
+
+    def repartir (self, mano, jugadores):
+        players = [] 
+        for p in range(jugadores):
+            players.append([])
+        for num_carta in range(mano):
+            for jugador in range(jugadores):
+                carta = self.naipes.pop(0)
+                players[jugador].append(carta)
+
+        return players
+
--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-from barajas import crear_baraja, barajar_for
+import barajas
 
-print(crear_baraja(['As', '2', '3', '4', '5', '6', '7','S', 'C', 'R'], ('O', 'C', 'E', 'B')))
+print(barajas.crear_baraja(['As', '2', '3', '4', '5', '6', '7','S', 'C', 'R'], ('O', 'C', 'E', 'B')))
 
 
-b = crear_baraja('A234567SCR', 'oceb')
+b = barajas.crear_baraja('A234567SCR', 'oceb')
 print(b)
-barajar_for(b)
+barajas.barajar_for(b)
+
+mi_baraja = barajas.Baraja('A234567SCR', 'oceb')
+print(mi_baraja.naipes)
